f1d21631674fd89993dafef8fee2f9cec9fd3c63
Jeel Patel <jeelpatel1912@gmail.com>
2020-12-08 02:07:47 -0500

    Added programmer comments, and starter code to method main
    Signed-off-by: Jeel Patel <jeelpatel1912@gmail.com>

--- src/Assignment04.java
+++ src/Assignment04.java
@@ -3,6 +3,13 @@ public class Assignment04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] primes = { 2, 3, 5, 7 };
+
+		for(int index = 0; index < primes.length; index++) {
+			System.out.println(primes[index]);
+		}
+		System.out.println("Program by Jeel Patel"); 
+
 
 	}
 
